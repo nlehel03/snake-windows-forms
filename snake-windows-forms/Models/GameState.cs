01538942c83946d05e00ffbb311943cb72cefe18
nlehel03 <nlehel03@gmail.com>
2025-10-07 14:18:43 +0200

    2025.10.07

---
+++ snake-windows-forms/Models/GameState.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace snake_windows_forms.Models
+{
+    public class GameState
+    {
+        public int n { get; }
+        public Snake snake { get; }
+        public Food food { get; private set; }
+        public int score { get; private set; }
+        public bool isGameOver { get; private set; }
+        Random r = new Random();
+
+        public GameState(int n)
+        {
+            this.n = n;
+            Point startPosition = new Point(n / 2, n / 2);
+            snake = new Snake(startPosition);
+            food = new Food(new Point(r.Next(0, n), r.Next(0, n)));
+            score = 0;
+            isGameOver = false;
+        }
+
+        public void Update()
+        {
+            if (isGameOver) return;
+            bool grow = snake.body[0].Equals(food.position);
+            snake.Move(grow);
+            if (grow)
+            {
+                score++;
+                food.Respawn(n, snake.body);
+            }
+            if (snake.IsCollisionWithSelf() || IsCollisionWithWall())
+            {
+                isGameOver = true;
+            }
+        }
+
+        private bool IsCollisionWithWall()
+        {
+            Point head = snake.body[0];
+            return head.X < 0 || head.X >= n || head.Y < 0 || head.Y >= n;
+        }
+
+
+    }
+
+}
